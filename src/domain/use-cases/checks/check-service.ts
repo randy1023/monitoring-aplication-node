495875e0d7ea8db0a4969ef70d23856da4541097
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-04-15 09:03:15 -0400

    mi primer commit

---
+++ src/domain/use-cases/checks/check-service.ts
@@ -0,0 +1,26 @@
+interface CheckServiceUseCase {
+  execute(url: string): Promise<boolean>;
+}
+type SuccesCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
+export class CheckService implements CheckServiceUseCase {
+  constructor(
+    private readonly succesCallback: SuccesCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
+  async execute(url: string): Promise<boolean> {
+    try {
+      const req = await fetch(url);
+      if (!req.ok) {
+        throw new Error(`Error on check service ${url}`);
+      }
+      this.succesCallback();
+      return true;
+    } catch (error) {
+      this.errorCallback(`${error}`);
+
+      return false;
+    }
+  }
+}
