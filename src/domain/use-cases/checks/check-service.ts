7128ec0a706fee2e7093625c7bd29ab545810efc
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-04-15 15:29:03 -0400

    inyeccion de repositorio en el caso de uso

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repositories/log.repository";
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -6,6 +9,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly succesCallback: SuccesCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -15,10 +19,15 @@ export class CheckService implements CheckServiceUseCase {
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
+      const log = new LogEntity(`${url} is working`, LogSeverityLevel.low);
+      this.logRepository.saveLog(log);
       this.succesCallback();
       return true;
     } catch (error) {
-      this.errorCallback(`${error}`);
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+      this.logRepository.saveLog(log);
+      this.errorCallback(errorMessage);
 
       return false;
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,20 @@
+import { LogDatasource } from "../domain/datasources/log.datasource";
+import { LogRepository } from "../domain/repositories/log.repository";
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { fileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogImplRepository } from "../infrastructure/repositories/log-impl.repository";
 import { CronService } from "./cron/cron-service";
-
+const fileSystemImpRepository = new LogImplRepository(
+  new fileSystemDatasource()
+);
 export class Server {
   public static start() {
     console.log("Server started...");
 
     CronService.createJob("*/5 * * * * *", () => {
-      const url = "https://google.com"
+      const url = "https://google.com";
       new CheckService(
+        fileSystemImpRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
